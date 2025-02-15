95bd2849e9f58de8a760ff68bb136418dbaad9cb
Dev's Alternate Github <devsalternategithub@gmail.com>
2025-02-15 10:26:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 import streamlit as st
 
-home = st.Page("page\\Home.py", icon='🏠')
+# home = st.Page("page\\Home.py", icon='🏠')
 
-view_research_paper = st.Page("page\\View_Research_Paper.py", icon='📄')
+# view_research_paper = st.Page("page\\View_Research_Paper.py", icon='📄')
 
-generate_analysis = st.Page("page\\Generate_Analysis.py", icon='📊')
+# generate_analysis = st.Page("page\\Generate_Analysis.py", icon='📊')
 
-credits = st.Page("page\\Credits.py", icon='📇')
+# credits = st.Page("page\\Credits.py", icon='📇')
 
 
-pg = st.navigation({
-    "Home": [home],
-    "Research Paper": [view_research_paper],
-    "Analysis": [generate_analysis],
-    "Credits": [credits]
-})
-pg.run()
+# pg = st.navigation({
+#     "Home": [home],
+#     "Research Paper": [view_research_paper],
+#     "Analysis": [generate_analysis],
+#     "Credits": [credits]
+# })
+# pg.run()
